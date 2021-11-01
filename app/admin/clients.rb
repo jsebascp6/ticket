a4a9d9f9a5f1c12b9d6ce6989c3420455eaef4a6
Sebastian Castro <jsebascp6@gmail.com>
2021-11-01 16:02:11 -0500

    fix admin clients

--- app/admin/clients.rb
+++ app/admin/clients.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Client do
   permit_params :first_name, :last_name, :residence_address, :phone, :document_id
+
+  form do |f|
+    f.inputs do
+      f.input :first_name
+      f.input :last_name
+      f.input :residence_address
+      f.input :phone
+      f.input :document_id
+    end
+
+    f.actions
+  end
 end
